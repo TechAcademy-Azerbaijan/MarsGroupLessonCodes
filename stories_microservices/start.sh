19d7e5814c4de778b1f829a31740bb9ca6ace136
idris <idris.sabanli@gmail.com>
2021-02-04 12:20:03 +0400

    api getaway

--- stories_microservices/start.sh
+++ stories_microservices/start.sh
@@ -3,12 +3,4 @@ docker-compose -f stories_microservices_auth_service/docker-compose.yml up -d &&
 docker-compose -f stories_microservices_post_service/docker-compose.yml  up -d &&
 docker-compose -f stories_microservices_subscribers_service/docker-compose.yml  up -d &&
 docker-compose -f stories_microservices_mailing_service/docker-compose.yml  up -d
-docker-compose -f deploy/docker-compose.yml up -d
-
-
-#api/v1.0/comments/
-#api/v1.0/auth/register
-#api/v1.0/post/
-#api/v1.0/subsribers/
-#
-#API Gateway
+docker-compose -f api_getaway/docker-compose.yml up -d
